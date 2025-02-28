3389b8a47200e1ea94687214714c2a510a7402d3
Dhruv Bindra <dhruv@manifold.inc>
2025-02-27 17:21:26 -0700

    feat: add Dockerfile
    Signed-off-by: Dhruv Bindra <dhruv@manifold.inc>

--- miner/main.go
+++ miner/main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"crypto/rand"
 	"crypto/rsa"
@@ -18,17 +17,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-const (
-	PrivateKey = "./keys/private_key.pem"
-	PublicKey  = "./keys/public_key.pem"
-)
-
 type ResponseBody struct {
 	Msg       Message `json:"msg"`
 	Signature []byte  `json:"signature"`
 }
 
 type Message struct {
+	Nonce        string    `json:"nonce"`
 	NumberOfGPUs int       `json:"no_of_gpus"`
 	GPUInfo      []GPUInfo `json:"gpu_info"`
 }
@@ -41,6 +36,11 @@ type GPUInfo struct {
 func main() {
 	e := echo.New()
 	e.POST("/", func(c echo.Context) error {
+		var request struct {
+			Nonce string `query:"nonce"`
+		}
+		c.Bind(&request)
+
 		ret := nvml.Init()
 		if ret != nvml.SUCCESS {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Unable to initialize NVML: %v", nvml.ErrorString(ret)))
@@ -63,21 +63,18 @@ func main() {
 			device, ret := nvml.DeviceGetHandleByIndex(i)
 			if ret != nvml.SUCCESS {
 				return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Unable to get device at index %d: %v", i, nvml.ErrorString(ret)))
-				log.Fatalf("Unable to get device at index %d: %v", i, nvml.ErrorString(ret))
 			}
 
 			arch, ret := device.GetName()
 			device.GetConfComputeGpuAttestationReport()
 			if ret != nvml.SUCCESS {
 				return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Unable to get arch of device at index %d: %v", i, nvml.ErrorString(ret)))
-				log.Fatalf("Unable to get arch of device at index %d: %v", i, nvml.ErrorString(ret))
 			}
 			info.GPUType = arch
 
 			uuid, ret := device.GetUUID()
 			if ret != nvml.SUCCESS {
 				return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Unable to get uuid of device at index %d: %v", i, nvml.ErrorString(ret)))
-				log.Fatalf("Unable to get uuid of device at index %d: %v", i, nvml.ErrorString(ret))
 			}
 			info.ID = uuid
 
@@ -85,6 +82,7 @@ func main() {
 		}
 
 		data := Message{
+			Nonce:        request.Nonce,
 			NumberOfGPUs: count,
 			GPUInfo:      gpuInfo,
 		}
@@ -94,14 +92,9 @@ func main() {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error converting to JSON: %v", err.Error()))
 		}
 
-		privKey, err := loadPrivateKey(PrivateKey)
+		privKey, err := loadPrivateKey()
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error generating private key: %v", err.Error()))
-		}
-
-		publicKey, err := loadPublicKey(PublicKey)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error generating public key: %v", err.Error()))
 		}
 
 		signature, err := sign(jsonData, privKey)
@@ -109,12 +102,9 @@ func main() {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error generating signature: %v", err.Error()))
 		}
 
-		if err = verify(jsonData, signature, publicKey); err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
-
 		resp := ResponseBody{
 			Msg: Message{
+				Nonce:        request.Nonce,
 				NumberOfGPUs: count,
 				GPUInfo:      gpuInfo,
 			},
@@ -135,42 +125,34 @@ func sign(msg []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
 	return signature, nil
 }
 
-func verify(msg []byte, signature []byte, publicKey *rsa.PublicKey) error {
-	hashed := sha256.Sum256(msg)
-	if err := rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashed[:], signature); err != nil {
-		return fmt.Errorf("verify signature: %v", err)
-	}
-	return nil
-}
-
-func loadPrivateKey(path string) (*rsa.PrivateKey, error) {
+func loadPrivateKey() (*rsa.PrivateKey, error) {
 	privateKeyData := []byte(`-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDpV7dCamvCP8a4
-a7eqOxy40kRSC1Nt8u7oEm1UyMXjJvo0cC1Ejpeb0qDPgxyHGK/uVbFZy1vGr2IT
-g9zDSTLEq8NqQ2p94SGP2ezRr1CqxE0uqmqwA0TMq6wzmmk62yMdDDXEJlp3mQv4
-Q6q+ONsjCYBeMS8Vjd/jFu1UYSgttex9OzIgGhHLQyGhBX0//2ceuAXXIBUZ1Vid
-UEqctRG7DUaaQCkj4+YKv/7jLA5XSdx70E5+pSAwy8478fUwg57MTUfwYDJeEWEm
-sG5WDM3AaHAoc3M8FWzHKtKpP5gW+wXKw+b2FEI6hNmvVVofnRePp5fZUud0ynfl
-DrK3Jj3RAgMBAAECggEAFp5Lq6bObXi6YeHCHsCGXRV3rSa/Jm0aEpmHqsRf5y/9
-6JOXlbnHMSHqd9bvDtraId2BWJGFYsXZGUhqv6zCnK0EHxKsGcnW859VjPFEHdtz
-mlKHTyqv8nFxGprs+Jkphw0qEIqBsi5eCkl/3Vq8RiILhQXaOSoEkdoE9Q0KZJy6
-jtmdq4DnYozQ5BSgXT4pqKiilg8M+TVXKaG1X8hBpE8HhsJAjJwLH0sGKgkjO46U
-AS7SwJbYtF3uHsSOE0ZMXKWKteBmxc1BgrMamkWrQtauLCye0uRQ3hXuOWp1CT2j
-5jJi67mwF23vQRcovNWKNlklOoCdlVfhEBRe+f/RCQKBgQD06fHuM0DsUuOX5Hr4
-b/GMwhRLjh/v8dSaTpzhfeg3V9vhFjmY9FGpqhpU7Et683P9XVdjgzb9nC0oslkP
-BRDM4H5QGxf6kMxIMp/rELLykgU9XzH5Rx7SPO/y+vHv7fIOoA1MxhICf9Q+Kmab
-sh+faQx/OptE6JGtsNCX2nhOGQKBgQDz568ZLIgcaPS+sAXRl15RShNMfBAltcJ6
-RmJpKoBpBpXphXcUKxgpt0cM3n1ZuvpiZn9T8+KqelwlFrhK8pFI+X+oHxBUCvH/
-9xZZULbZJAsTW/1/js/Bd5Cca0uJwd/UlRWSEWXZOc4F3EXHrnR0rK8ce5A299Hf
-LQwG6NZ0eQKBgQC568rr796uSHJcAWfUp25wU3kCrl7SUPv7NAhmKaWaNclGgw+w
-bHB94xLgOw1FxgDcavqHIboiJmglx9ZRz5+kWBurhhXa8gcSG9RIp0GhYXY208hl
-GzdrlDT3eMcnQ7QIpDnUeVx8pzuhAaEpOrxE0INbJktusTq950dCr+e6KQKBgQDj
-TYNtHocqktWj8D87+Kk5cKNToDscKau4cGe+QpGxTyXenzc+YHsARYG9iIuhWBOI
-KKV5A8y1u5qsJeRSVIT5USua7ng1Go41UXxOeF4vNtvristQWTNcxaTLYWLgWwHS
-B642lmgobSqHY6TUVszI/zWigbUAxSudpUmUFuHlYQKBgFc4YEZ81WzwK3wGBJqL
-8W49Y03JjoQRlTgrJnrH9rZeUeQnHl4VIQoyWecyH1uw0U7WsSfVcwAEoriVXQ72
-+jYGDkLKE5g5bnSav+qdO1ma7QB1TcyCMLdOxwdexqpM/kJj0Rkms6+xa+Y9YjLd
-jhtO/eFXEESMvhDW2AewTuW9
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDEzrG4C9s86Pu+
+nZdeUk1iCOwXI4BH41an3atHmxtKtypsqiAC6ehYRDqmOt2wtW/uGNTNPS46PZvi
+MMHQhLZh1uMeajedi+mypqrOnd6ezNaIk5vvP8+M8qngqH7j4Hrpe0Qqk0fQ5Jt7
+RgsbUPGW8mFwoIQeb/mmnyj7ceYXblPFyjeAf1YArjp+J9aDH1+IALHDA4Y6P1VT
+DfZnmWGqkaQxYSQ/lWJGscVQx9SYo09KPLeNJJT9hMvYWx3yLFTuuMFGBHNsQRuF
+9iXYoZrza71+oIArO2fOeT/grJZhdo6ATOPcKNr+Fh1ZJ2pd+OsIwEKMLcwVAuUU
+GfBmk/2VAgMBAAECggEAL4GDJZ17DM9iYScyMBrmwfjMYGN55iBqvPeFjjHTGyxu
+ia9hLcLfilrh21lPRQXu4EbPK0GO5JEVjTpetmIqkiBoT2GtThLFVRHlHh2+bI+e
+Y3ZaCBzbkTbMKcYSZxg7IxvVnv9RMQvVuiJ0sOHPhzO3sJOWgLBfz0gRAO0D8PiM
+0WdkyEaHfko9LvbUboaI37UJErdGrxbvuI0WyJCrju3iYCPC7KUYTejStWSS3iRL
+dQDboSR+9w7KA+TYJG7cSVNx6pPRY4hJoq5RjP5HnBcgv0TGTdkzEJ/7KR60thr8
+LOVV0cs8mIr/mhcXiY4EkQdCicbz9aBMRWcwYbbLMQKBgQDjmnj3GQq1P9BH7Wd4
+PMDWNIEa7EJtxu2s0s585ykYUS7SaOsrDU5zaEJL84XyQDXH4sqdCBk0l7ekWN1J
+fzj8hPnPpVoo6mABTH3JmZpUrVA3b9CbnRdDAomoiHjgtjPCBatuR6gd9tXckNGz
+2NUuqT3EX1IHYBOzVitTX4xn3QKBgQDdXJ03tAjU4V2rNpcQX9dXQvHyYzzOATjO
+C3H/zm5kI0XAKv6FVK5O3zGnoHFZ+sbY0mIQhNUAXIQ+GiJjxgHsG7fJiPIkZElh
+UKLgUr3UCxPpkv2p+wBPEXzgYgTU3SouNyxsbUBNHQHFFOZC+Wb9FAuQ8DTVV1ae
+Y2x15kwtGQKBgQDQtRW3gH8RNTMIwenfl9Y8lmjNB1ji0Ueg8q3mWxKT+d21lAAR
+umH18eTZQLXTLo4ifRTOal6ym20Kh3JjswXOiSGKB1ZlroXaqwmtk+yxOTwVmbLw
+/1wVVn5kzMALTwinIgvRVGQVFgOF/kZNZMgXwZmiOMY/fRqTmLCiAzv9AQKBgEd4
+p6pcjp1tRnkJXy0CRNz/2G6SbAd3jYe5Wk1lsW7TlKm/N/6EoOMGXVnqC68psRpA
+kzkSKlO7i1chyQi93dEGAgkoFkZ5uteY2X3kOTZFfWBk/VHsoOTmmJM6EqVH416Y
+oddATwqW2wNvBAmLzRzZZAKgC+72DBmHsHVArkfBAoGAU59WsaU1m12H8mdttbGx
+JVE6aY5peLbgns8KMu/gDSbuEMATtRxTpxkXOMndCTK2unnD9QJ5YmBpq65tX78j
+KC05fc8rMM2jaAN5g5FsxArPnG7TZN4IHoEA2g0cbKAB0ZqO3YmyorMNHZqhz1Cp
+URwBrTk8O5uR4+lQuGSVHY0=
 -----END PRIVATE KEY-----
 `)
 
@@ -189,29 +171,6 @@ jhtO/eFXEESMvhDW2AewTuW9
 		return nil, fmt.Errorf("type assertion to rsa.PrivateKey failed")
 	}
 	return privKey, nil
-}
-
-func loadPublicKey(path string) (*rsa.PublicKey, error) {
-	publicKeyData, err := os.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("load public key: %v", err)
-	}
-
-	block, _ := pem.Decode(publicKeyData)
-	if block == nil {
-		return nil, fmt.Errorf("parse PEM block: key not found")
-	}
-
-	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, fmt.Errorf("parse public key: %v", err)
-	}
-
-	rsaPub, ok := pub.(*rsa.PublicKey)
-	if !ok {
-		return nil, fmt.Errorf("type assertion to rsa.PublicKey failed")
-	}
-	return rsaPub, nil
 }
 
 // openssl genpkey -algorithm RSA -out ./keys/private_key.pem
