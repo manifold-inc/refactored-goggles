835e7b53ed70aa243a0dff31e9b54aac1bfa9d41
josh <josh@mjbrowns.com>
2025-03-01 10:45:19 -0600

    fix: shard rsa key

--- miner/main.go
+++ miner/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"crypto"
-	"encoding/json"
-	"encoding/pem"
-	"fmt"
-	"log"
-	"net/http"
-
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
 	"github.com/labstack/echo/v4"
@@ -35,6 +35,7 @@ type GPUInfo struct {
 
 func main() {
 	e := echo.New()
+	e.Logger.SetOutput(io.Discard)
 	e.POST("/", func(c echo.Context) error {
 		var request struct {
 			Nonce string `query:"nonce"`
@@ -92,7 +93,16 @@ func main() {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error converting to JSON: %v", err.Error()))
 		}
 
-		privKey, err := loadPrivateKey()
+		const middle2 = `
+Y3ZaCBzbkTbMKcYSZxg7IxvVnv9RMQvVuiJ0sOHPhzO3sJOWgLBfz0gRAO0D8PiM
+0WdkyEaHfko9LvbUboaI37UJErdGrxbvuI0WyJCrju3iYCPC7KUYTejStWSS3iRL
+dQDboSR+9w7KA+TYJG7cSVNx6pPRY4hJoq5RjP5HnBcgv0TGTdkzEJ/7KR60thr8
+LOVV0cs8mIr/mhcXiY4EkQdCicbz9aBMRWcwYbbLMQKBgQDjmnj3GQq1P9BH7Wd4
+PMDWNIEa7EJtxu2s0s585ykYUS7SaOsrDU5zaEJL84XyQDXH4sqdCBk0l7ekWN1J
+fzj8hPnPpVoo6mABTH3JmZpUrVA3b9CbnRdDAomoiHjgtjPCBatuR6gd9tXckNGz
+2NUuqT3EX1IHYBOzVitTX4xn3QKBgQDdXJ03tAjU4V2rNpcQX9dXQvHyYzzOATjO
+`
+		privKey, err := loadPrivateKey(middle2)
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error generating private key: %v", err.Error()))
 		}
@@ -125,8 +135,7 @@ func sign(msg []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
 	return signature, nil
 }
 
-func loadPrivateKey() (*rsa.PrivateKey, error) {
-	privateKeyData := []byte(`-----BEGIN PRIVATE KEY-----
+const middle1 = `
 MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDEzrG4C9s86Pu+
 nZdeUk1iCOwXI4BH41an3atHmxtKtypsqiAC6ehYRDqmOt2wtW/uGNTNPS46PZvi
 MMHQhLZh1uMeajedi+mypqrOnd6ezNaIk5vvP8+M8qngqH7j4Hrpe0Qqk0fQ5Jt7
@@ -135,24 +144,24 @@ DfZnmWGqkaQxYSQ/lWJGscVQx9SYo09KPLeNJJT9hMvYWx3yLFTuuMFGBHNsQRuF
 9iXYoZrza71+oIArO2fOeT/grJZhdo6ATOPcKNr+Fh1ZJ2pd+OsIwEKMLcwVAuUU
 GfBmk/2VAgMBAAECggEAL4GDJZ17DM9iYScyMBrmwfjMYGN55iBqvPeFjjHTGyxu
 ia9hLcLfilrh21lPRQXu4EbPK0GO5JEVjTpetmIqkiBoT2GtThLFVRHlHh2+bI+e
-Y3ZaCBzbkTbMKcYSZxg7IxvVnv9RMQvVuiJ0sOHPhzO3sJOWgLBfz0gRAO0D8PiM
-0WdkyEaHfko9LvbUboaI37UJErdGrxbvuI0WyJCrju3iYCPC7KUYTejStWSS3iRL
-dQDboSR+9w7KA+TYJG7cSVNx6pPRY4hJoq5RjP5HnBcgv0TGTdkzEJ/7KR60thr8
-LOVV0cs8mIr/mhcXiY4EkQdCicbz9aBMRWcwYbbLMQKBgQDjmnj3GQq1P9BH7Wd4
-PMDWNIEa7EJtxu2s0s585ykYUS7SaOsrDU5zaEJL84XyQDXH4sqdCBk0l7ekWN1J
-fzj8hPnPpVoo6mABTH3JmZpUrVA3b9CbnRdDAomoiHjgtjPCBatuR6gd9tXckNGz
-2NUuqT3EX1IHYBOzVitTX4xn3QKBgQDdXJ03tAjU4V2rNpcQX9dXQvHyYzzOATjO
+`
+
+func loadPrivateKey(middle2 string) (*rsa.PrivateKey, error) {
+	const prefix = "-----BEGIN PRIVATE KEY-----\n"
+	last := `p6pcjp1tRnkJXy0CRNz/2G6SbAd3jYe5Wk1lsW7TlKm/N/6EoOMGXVnqC68psRpA
+kzkSKlO7i1chyQi93dEGAgkoFkZ5uteY2X3kOTZFfWBk/VHsoOTmmJM6EqVH416Y
+oddATwqW2wNvBAmLzRzZZAKgC+72DBmHsHVArkfBAoGAU59WsaU1m12H8mdttbGx
+JVE6aY5peLbgns8KMu/gDSbuEMATtRxTpxkXOMndCTK2unnD9QJ5YmBpq65tX78j
+KC05fc8rMM2jaAN5g5FsxArPnG7TZN4IHoEA2g0cbKAB0ZqO3YmyorMNHZqhz1Cp
+URwBrTk8O5uR4+lQuGSVHY0=`
+
+	privateKeyData := []byte(prefix + middle1 + middle2 + `
 C3H/zm5kI0XAKv6FVK5O3zGnoHFZ+sbY0mIQhNUAXIQ+GiJjxgHsG7fJiPIkZElh
 UKLgUr3UCxPpkv2p+wBPEXzgYgTU3SouNyxsbUBNHQHFFOZC+Wb9FAuQ8DTVV1ae
 Y2x15kwtGQKBgQDQtRW3gH8RNTMIwenfl9Y8lmjNB1ji0Ueg8q3mWxKT+d21lAAR
 umH18eTZQLXTLo4ifRTOal6ym20Kh3JjswXOiSGKB1ZlroXaqwmtk+yxOTwVmbLw
 /1wVVn5kzMALTwinIgvRVGQVFgOF/kZNZMgXwZmiOMY/fRqTmLCiAzv9AQKBgEd4
-p6pcjp1tRnkJXy0CRNz/2G6SbAd3jYe5Wk1lsW7TlKm/N/6EoOMGXVnqC68psRpA
-kzkSKlO7i1chyQi93dEGAgkoFkZ5uteY2X3kOTZFfWBk/VHsoOTmmJM6EqVH416Y
-oddATwqW2wNvBAmLzRzZZAKgC+72DBmHsHVArkfBAoGAU59WsaU1m12H8mdttbGx
-JVE6aY5peLbgns8KMu/gDSbuEMATtRxTpxkXOMndCTK2unnD9QJ5YmBpq65tX78j
-KC05fc8rMM2jaAN5g5FsxArPnG7TZN4IHoEA2g0cbKAB0ZqO3YmyorMNHZqhz1Cp
-URwBrTk8O5uR4+lQuGSVHY0=
+` + last + "\n" + `
 -----END PRIVATE KEY-----
 `)
 
