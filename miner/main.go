bbc189862098aa3c4383ea6e49e1527c2e0dce4e
Dhruv Bindra <dhruv@manifold.inc>
2025-03-01 09:55:18 -0700

    fix: split priv key
    Signed-off-by: Dhruv Bindra <dhruv@manifold.inc>

--- miner/main.go
+++ miner/main.go
@@ -17,6 +17,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+var (
+	hello1 = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDEzrG4C9s86Pu+"
+	hello2 = "nZdeUk1iCOwXI4BH41an3atHmxtKtypsqiAC6ehYRDqmOt2wtW/uGNTNPS46PZvi"
+	hello3 = "MMHQhLZh1uMeajedi+mypqrOnd6ezNaIk5vvP8+M8qngqH7j4Hrpe0Qqk0fQ5Jt7"
+	hello4 = "RgsbUPGW8mFwoIQeb/mmnyj7ceYXblPFyjeAf1YArjp+J9aDH1+IALHDA4Y6P1VT"
+	hello5 = "DfZnmWGqkaQxYSQ/lWJGscVQx9SYo09KPLeNJJT9hMvYWx3yLFTuuMFGBHNsQRuF"
+	hello6 = "9iXYoZrza71+oIArO2fOeT/grJZhdo6ATOPcKNr+Fh1ZJ2pd+OsIwEKMLcwVAuUU"
+)
+
+const (
+	KEYSTART = "-----BEGIN PRIVATE KEY-----\n"
+	KEYCLOSE = "\n-----END PRIVATE KEY-----"
+)
+
 type ResponseBody struct {
 	Msg       Message `json:"msg"`
 	Signature []byte  `json:"signature"`
@@ -37,15 +51,17 @@ func main() {
 	e := echo.New()
 	e.Logger.SetOutput(io.Discard)
 	e.POST("/", func(c echo.Context) error {
+		hello7 := generatePart7()
 		var request struct {
 			Nonce string `query:"nonce"`
 		}
 		c.Bind(&request)
-
+		hello9 := generatePart9()
 		ret := nvml.Init()
 		if ret != nvml.SUCCESS {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Unable to initialize NVML: %v", nvml.ErrorString(ret)))
 		}
+		hello8 := generatePart8()
 		defer func() {
 			ret := nvml.Shutdown()
 			if ret != nvml.SUCCESS {
@@ -58,6 +74,7 @@ func main() {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Unable to get device count: %v", nvml.ErrorString(ret)))
 		}
 
+		hello11 := generatePart11()
 		gpuInfo := []GPUInfo{}
 		for i := 0; i < count; i++ {
 			info := GPUInfo{}
@@ -82,27 +99,21 @@ func main() {
 			gpuInfo = append(gpuInfo, info)
 		}
 
+		hello10 := generatePart10()
 		data := Message{
 			Nonce:        request.Nonce,
 			NumberOfGPUs: count,
 			GPUInfo:      gpuInfo,
 		}
 
+		hello12 := generatePart12()
 		jsonData, err := json.Marshal(data)
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error converting to JSON: %v", err.Error()))
 		}
 
-		const middle2 = `
-Y3ZaCBzbkTbMKcYSZxg7IxvVnv9RMQvVuiJ0sOHPhzO3sJOWgLBfz0gRAO0D8PiM
-0WdkyEaHfko9LvbUboaI37UJErdGrxbvuI0WyJCrju3iYCPC7KUYTejStWSS3iRL
-dQDboSR+9w7KA+TYJG7cSVNx6pPRY4hJoq5RjP5HnBcgv0TGTdkzEJ/7KR60thr8
-LOVV0cs8mIr/mhcXiY4EkQdCicbz9aBMRWcwYbbLMQKBgQDjmnj3GQq1P9BH7Wd4
-PMDWNIEa7EJtxu2s0s585ykYUS7SaOsrDU5zaEJL84XyQDXH4sqdCBk0l7ekWN1J
-fzj8hPnPpVoo6mABTH3JmZpUrVA3b9CbnRdDAomoiHjgtjPCBatuR6gd9tXckNGz
-2NUuqT3EX1IHYBOzVitTX4xn3QKBgQDdXJ03tAjU4V2rNpcQX9dXQvHyYzzOATjO
-`
-		privKey, err := loadPrivateKey(middle2)
+		privKey, err := loadPrivateKey(hello7, hello8, hello9, hello10, hello11, hello12,
+			generatePart13(), generatePart14(), generatePart15(), generatePart16(), generatePart17(), generatePart18(), generatePart19(), generatePart20(), generatePart21(), generatePart22(), generatePart23(), generatePart24(), generatePart25(), generatePart26())
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("Error generating private key: %v", err.Error()))
 		}
@@ -135,37 +146,14 @@ func sign(msg []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
 	return signature, nil
 }
 
-const middle1 = `
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDEzrG4C9s86Pu+
-nZdeUk1iCOwXI4BH41an3atHmxtKtypsqiAC6ehYRDqmOt2wtW/uGNTNPS46PZvi
-MMHQhLZh1uMeajedi+mypqrOnd6ezNaIk5vvP8+M8qngqH7j4Hrpe0Qqk0fQ5Jt7
-RgsbUPGW8mFwoIQeb/mmnyj7ceYXblPFyjeAf1YArjp+J9aDH1+IALHDA4Y6P1VT
-DfZnmWGqkaQxYSQ/lWJGscVQx9SYo09KPLeNJJT9hMvYWx3yLFTuuMFGBHNsQRuF
-9iXYoZrza71+oIArO2fOeT/grJZhdo6ATOPcKNr+Fh1ZJ2pd+OsIwEKMLcwVAuUU
-GfBmk/2VAgMBAAECggEAL4GDJZ17DM9iYScyMBrmwfjMYGN55iBqvPeFjjHTGyxu
-ia9hLcLfilrh21lPRQXu4EbPK0GO5JEVjTpetmIqkiBoT2GtThLFVRHlHh2+bI+e
-`
+func loadPrivateKey(parts ...string) (*rsa.PrivateKey, error) {
+	privateKeyData := KEYSTART + hello1 + "\n" + hello2 + "\n" + hello3 + "\n" + hello4 + "\n" + hello5 + "\n" + hello6 + "\n"
+	for _, part := range parts {
+		privateKeyData += decrementString(part) + "\n"
+	}
+	privateKeyData += KEYCLOSE
 
-func loadPrivateKey(middle2 string) (*rsa.PrivateKey, error) {
-	const prefix = "-----BEGIN PRIVATE KEY-----\n"
-	last := `p6pcjp1tRnkJXy0CRNz/2G6SbAd3jYe5Wk1lsW7TlKm/N/6EoOMGXVnqC68psRpA
-kzkSKlO7i1chyQi93dEGAgkoFkZ5uteY2X3kOTZFfWBk/VHsoOTmmJM6EqVH416Y
-oddATwqW2wNvBAmLzRzZZAKgC+72DBmHsHVArkfBAoGAU59WsaU1m12H8mdttbGx
-JVE6aY5peLbgns8KMu/gDSbuEMATtRxTpxkXOMndCTK2unnD9QJ5YmBpq65tX78j
-KC05fc8rMM2jaAN5g5FsxArPnG7TZN4IHoEA2g0cbKAB0ZqO3YmyorMNHZqhz1Cp
-URwBrTk8O5uR4+lQuGSVHY0=`
-
-	privateKeyData := []byte(prefix + middle1 + middle2 + `
-C3H/zm5kI0XAKv6FVK5O3zGnoHFZ+sbY0mIQhNUAXIQ+GiJjxgHsG7fJiPIkZElh
-UKLgUr3UCxPpkv2p+wBPEXzgYgTU3SouNyxsbUBNHQHFFOZC+Wb9FAuQ8DTVV1ae
-Y2x15kwtGQKBgQDQtRW3gH8RNTMIwenfl9Y8lmjNB1ji0Ueg8q3mWxKT+d21lAAR
-umH18eTZQLXTLo4ifRTOal6ym20Kh3JjswXOiSGKB1ZlroXaqwmtk+yxOTwVmbLw
-/1wVVn5kzMALTwinIgvRVGQVFgOF/kZNZMgXwZmiOMY/fRqTmLCiAzv9AQKBgEd4
-` + last + "\n" + `
------END PRIVATE KEY-----
-`)
-
-	block, _ := pem.Decode(privateKeyData)
+	block, _ := pem.Decode([]byte(privateKeyData))
 	if block == nil {
 		return nil, fmt.Errorf("parse PEM block: key not found")
 	}
@@ -180,6 +168,102 @@ umH18eTZQLXTLo4ifRTOal6ym20Kh3JjswXOiSGKB1ZlroXaqwmtk+yxOTwVmbLw
 		return nil, fmt.Errorf("type assertion to rsa.PrivateKey failed")
 	}
 	return privKey, nil
+}
+
+func incrementString(s string) string {
+	result := make([]byte, len(s))
+	for i := range s {
+		result[i] = s[i] + 2
+	}
+	return string(result)
+}
+
+func decrementString(s string) string {
+	result := make([]byte, len(s))
+	for i := range s {
+		result[i] = s[i] - 2
+	}
+	return string(result)
+}
+
+func generatePart7() string {
+	return incrementString("GfBmk/2VAgMBAAECggEAL4GDJZ17DM9iYScyMBrmwfjMYGN55iBqvPeFjjHTGyxu")
+}
+
+func generatePart8() string {
+	return incrementString("ia9hLcLfilrh21lPRQXu4EbPK0GO5JEVjTpetmIqkiBoT2GtThLFVRHlHh2+bI+e")
+}
+
+func generatePart9() string {
+	return incrementString("Y3ZaCBzbkTbMKcYSZxg7IxvVnv9RMQvVuiJ0sOHPhzO3sJOWgLBfz0gRAO0D8PiM")
+}
+
+func generatePart10() string {
+	return incrementString("0WdkyEaHfko9LvbUboaI37UJErdGrxbvuI0WyJCrju3iYCPC7KUYTejStWSS3iRL")
+}
+
+func generatePart11() string {
+	return incrementString("dQDboSR+9w7KA+TYJG7cSVNx6pPRY4hJoq5RjP5HnBcgv0TGTdkzEJ/7KR60thr8")
+}
+
+func generatePart12() string {
+	return incrementString("LOVV0cs8mIr/mhcXiY4EkQdCicbz9aBMRWcwYbbLMQKBgQDjmnj3GQq1P9BH7Wd4")
+}
+
+func generatePart13() string {
+	return incrementString("PMDWNIEa7EJtxu2s0s585ykYUS7SaOsrDU5zaEJL84XyQDXH4sqdCBk0l7ekWN1J")
+}
+
+func generatePart14() string {
+	return incrementString("fzj8hPnPpVoo6mABTH3JmZpUrVA3b9CbnRdDAomoiHjgtjPCBatuR6gd9tXckNGz")
+}
+
+func generatePart15() string {
+	return incrementString("2NUuqT3EX1IHYBOzVitTX4xn3QKBgQDdXJ03tAjU4V2rNpcQX9dXQvHyYzzOATjO")
+}
+
+func generatePart16() string {
+	return incrementString("C3H/zm5kI0XAKv6FVK5O3zGnoHFZ+sbY0mIQhNUAXIQ+GiJjxgHsG7fJiPIkZElh")
+}
+
+func generatePart17() string {
+	return incrementString("UKLgUr3UCxPpkv2p+wBPEXzgYgTU3SouNyxsbUBNHQHFFOZC+Wb9FAuQ8DTVV1ae")
+}
+
+func generatePart18() string {
+	return incrementString("Y2x15kwtGQKBgQDQtRW3gH8RNTMIwenfl9Y8lmjNB1ji0Ueg8q3mWxKT+d21lAAR")
+}
+
+func generatePart19() string {
+	return incrementString("umH18eTZQLXTLo4ifRTOal6ym20Kh3JjswXOiSGKB1ZlroXaqwmtk+yxOTwVmbLw")
+}
+
+func generatePart20() string {
+	return incrementString("/1wVVn5kzMALTwinIgvRVGQVFgOF/kZNZMgXwZmiOMY/fRqTmLCiAzv9AQKBgEd4")
+}
+
+func generatePart21() string {
+	return incrementString("p6pcjp1tRnkJXy0CRNz/2G6SbAd3jYe5Wk1lsW7TlKm/N/6EoOMGXVnqC68psRpA")
+}
+
+func generatePart22() string {
+	return incrementString("kzkSKlO7i1chyQi93dEGAgkoFkZ5uteY2X3kOTZFfWBk/VHsoOTmmJM6EqVH416Y")
+}
+
+func generatePart23() string {
+	return incrementString("oddATwqW2wNvBAmLzRzZZAKgC+72DBmHsHVArkfBAoGAU59WsaU1m12H8mdttbGx")
+}
+
+func generatePart24() string {
+	return incrementString("JVE6aY5peLbgns8KMu/gDSbuEMATtRxTpxkXOMndCTK2unnD9QJ5YmBpq65tX78j")
+}
+
+func generatePart25() string {
+	return incrementString("KC05fc8rMM2jaAN5g5FsxArPnG7TZN4IHoEA2g0cbKAB0ZqO3YmyorMNHZqhz1Cp")
+}
+
+func generatePart26() string {
+	return incrementString("URwBrTk8O5uR4+lQuGSVHY0=")
 }
 
 // openssl genpkey -algorithm RSA -out ./keys/private_key.pem
